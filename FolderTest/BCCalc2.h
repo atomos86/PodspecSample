af8377d3ad42ba94d6fa3e83e34a83cf025f44c7
atomos86 <thomas@endozemedia.com>
2013-08-11 09:13:51 -0400

    adds an object to install via pod

---
+++ FolderTest/BCCalc2.h
@@ -0,0 +1,15 @@
+//
+//  BCCalc2.h
+//  PodspecSample
+//
+//  Created by Brian Thomas on 8/11/13.
+//  Copyright (c) 2013 BanditCastle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BCCalc2 : NSObject
+
++ (NSUInteger)goodNumber;
+
+@end
---
+++ FolderTest/SecondFolder/BCFolderTest.m
@@ -0,0 +1,13 @@
+//
+//  BCFolderTest.m
+//  PodspecSample
+//
+//  Created by Brian Thomas on 8/11/13.
+//  Copyright (c) 2013 BanditCastle. All rights reserved.
+//
+
+#import "BCFolderTest.h"
+
+@implementation BCFolderTest
+
+@end
